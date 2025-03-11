fd6ef20715be97faad111bf1e2b599c78d45ef5d
joshmasterton <joshmasterton@tuta.io>
2025-03-11 06:07:04 +0000

    Update CI/CD workflow

---
+++ server/dist/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+// Initalize app
+export const app = express();
+const { PORT, NODE_ENV } = process.env;
+// Welcome to server
+app.get("/", (_req, res) => {
+  res.json({ message: "Welcome to game" });
+});
+// Listen to server
+if (NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Listening to server on port ${PORT}`);
+  });
+}
